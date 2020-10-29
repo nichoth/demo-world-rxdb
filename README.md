7666b6bbd567b8a51ad4abd7edc5f28e2e5357c2
nichoth <nichoth@gmail.com>
2020-10-29 14:59:04 -0700

    more stuff

---
+++ README.md
@@ -0,0 +1,8 @@
+> To synchronize data between your clients and your server, RxDB provides replication modules for CouchDB and GraphQL.
+
+The hasura demo uses a postgres DB from heroku
+
+-------------------------------------------------
+
+Doing what it says here: https://rxdb.info/rx-schema.html
+
--- src/index.js
+++ src/index.js
@@ -1,1 +1,26 @@
-console.log('fooo')
+import {
+  createRxDatabase,
+  RxDatabase,
+  addRxPlugin
+} from 'rxdb'
+var pouch = require('pouchdb-adapter-idb')
+var myHeroSchema = require('./heros.json')
+
+addRxPlugin(pouch)
+
+createRxDatabase({
+    name: 'heroesdb',           // <- name
+    adapter: 'idb',          // <- storage-adapter
+    password: 'myPassword',     // <- password (optional)
+    multiInstance: true,         // <- multiInstance (optional, default: true)
+    eventReduce: false // <- eventReduce (optional, default: true)
+}).then(async function (db) {
+    console.log('db', db)
+    db.$.subscribe(changeEvent => console.log(changeEvent));
+
+    await db.collection({
+        name: 'heroes',
+        schema: myHeroSchema
+    });
+    console.log('name', db.heroes.name);
+})
